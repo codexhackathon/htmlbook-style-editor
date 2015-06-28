8e6e989733463bd4fdc66cd7c53c1fb5449a0267
Tim Farnam <twfarnam@gmail.com>
2015-06-27 17:49:51 -0700

    added separate js/css and iframe

---
+++ main.js
@@ -0,0 +1,27 @@
+$(function() {
+
+  $('').on('change', function() {
+
+    $.get('./examples/Frankenstein.html', {
+      error: function(response) {
+        console.log(response);
+        alert('couldn\'t load story');
+      },
+      success: function(data) {
+        $('#htmlbookcontent').html(data);
+      }
+    });
+
+  });
+
+
+  $('style').on('keypress', function(e) {
+
+    // ctrl-enter to apply style
+    if (e.which === 13 && e.ctrlKey) {
+      
+    }
+
+  });
+
+});
