09ba2e3d0330aa47e98a97366c1a1e992d6d4ad4
Seth Woodworth <seth@sethish.com>
2015-06-28 18:49:00 -0400

    implements save

--- main.js
+++ main.js
@@ -70,6 +70,24 @@ $(function() {
     loadStyle(index);
   });
 
+  function download(filename, text) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
+  }
+
+  $('#save').click(function() { download('style.css', editor.getValue())});
+  //onClick="javascriot: download('style.css', editor.getValue())"
+
 
 
 });
