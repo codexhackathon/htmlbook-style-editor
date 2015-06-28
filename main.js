6f85ff743269eff86d79833cfc3f0185792860c9
Tim Farnam <twfarnam@gmail.com>
2015-06-27 18:15:26 -0700

    dropdown and ctrl-enter working

--- main.js
+++ main.js
@@ -1,28 +1,40 @@
 $(function() {
 
-  $('.selection.dropdown')
-    .dropdown('setting', 'transition', 'vertical flip');
+  $('.selection.dropdown').dropdown('setting', 'transition', 'vertical flip');
 
-  $('').on('change', function() {
+  var books = [
+    'examples/Pride_and_Prejudice.html',
+    'examples/Frankenstein.html',
+    'examples/Moby-Dick.html',
+  ];
 
-    $.get('./examples/Frankenstein.html', {
+  var loadBook = function(index) {
+    $.ajax(books[index], {
       error: function(response) {
         console.log(response);
         alert('couldn\'t load story');
       },
       success: function(data) {
-        $('#htmlbookcontent').html(data);
+        $('iframe#html-book-content').contents().find('body').html(data);
       }
     });
+  };
 
+  loadBook(1);
+
+  $('select').on('change', function() {
+    var index = $('select').val() * 1;
+    loadBook(index);
   });
 
 
-  $('style').on('keypress', function(e) {
+  $('style[contenteditable]').on('keypress', function(e) {
 
     // ctrl-enter to apply style
     if (e.which === 13 && e.ctrlKey) {
-      
+      e.preventDefault();
+      var style = $('style[contenteditable]').text();
+      $('iframe').contents().find('head').html('<style>' + style + '</style>');
     }
 
   });
