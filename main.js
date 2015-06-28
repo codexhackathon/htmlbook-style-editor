fc1906be8807e8b27643ff9f23bb14b3b0569406
Seth Woodworth <seth@sethish.com>
2015-06-28 18:08:44 -0400

    maybe fixes mac bindings

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(function() {
 
     editor.commands.addCommand({
       name: 'Apply style to iframe',
-      bindKey: {win: 'Ctrl-s',  Mac: 'Ctrl-s'},
+      bindKey: {win: 'Ctrl-s',  Mac: 'Command-S'},
       exec: function(editor){applyStyle(editor)},
       readOnly: true // false if this command should not apply in readOnly mode
     });
